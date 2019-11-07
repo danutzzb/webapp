1edaf40f0a71b4e332b3bb6f52be76cbab5b2c89
Dan <Dan@Dan-PC>
2019-11-07 10:29:12 +0200

    Home Session bun

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 require_once 'config.php';
 session_start();
-//$ses = $_SESSION['username'];
+$ses = $_SESSION['username'];
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if(isset($_POST['submit']) && isset($_POST['text']) ){
 $body = $_POST['text'];
-$query = "insert into webapp.comentarii(comment) values ('$body') ";
+$query = "insert into webapp.comentarii(user_id,comment)
+    VALUES ((SELECT user_id FROM utilizatori WHERE username = '$ses'), '$body')";
 $data = mysqli_query($conn, $query);
 //echo $body;
 }
@@ -56,6 +57,12 @@ $data = mysqli_query($conn, $query);
         margin: 5px;
         padding: 3px;
     }
+    #comment {
+        background-color: #ccccff;
+        border: 1px solid aliceblue;
+        margin: 3px;
+        padding: 3px;
+    }
     
 </style>
 <html>
@@ -82,16 +89,31 @@ $data = mysqli_query($conn, $query);
                         echo $value;
                         echo "</div>";
                         echo "<br>";
+                        $query = "select comment from comentarii";
+                        $com = mysqli_query($conn, $query);
+                
+                        $comment = $com;
+                        while ($comment = $com->fetch_assoc()){
+                            foreach ($comment as $key=>$value){
+                            echo "<div id=".'comment'.">";
+                            echo $value;
+                            echo "</div>";
+                            echo "<br>";
+                        
+                            
+                    };
+                };
                         echo "<form action='' method='post'>
                             <textarea name='text' rows='5' cols='100' placeholder='Comentariul tau aici' ></textarea>
                             <button type='submit' name='submit'>Posteaza Comentariu</button></form>";
                             
                     };
                 };
+                
+                
+                //var_dump($com)
                 ?>
-<!--                <form action="" method="post">
-                            <textarea name="text" rows="5" cols="100" placeholder="Comentariul tau aici" ></textarea>
-                            <input type="submit" value="Posteaza Comentariu" name="submit">-->
+
                 
             </article>
         </div>
